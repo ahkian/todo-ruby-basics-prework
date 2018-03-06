6cfbb15fa65516bc62c208a6941d1ae944544b61
Daniel Brazel <dbrazel4@gmail.com>
2018-03-06 20:39:48 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,19 +3,19 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  name = value 
+  name = value
 end
 
-def argue
+def argue(s)
+  s
+end
+
+def greeting(name, other)
   
 end
 
-def greeting
-
-end
-
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
